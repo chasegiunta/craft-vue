69e93e64259a44153b3b6fd72de239162298bd02
Chase Giunta <me@chasegiunta.com>
2019-06-06 19:56:27 -0500

    Configure Babel - Add hints & links to documentation

---
+++ babel.config.js
@@ -0,0 +1,17 @@
+// https://cli.vuejs.org/guide/browser-compatibility.html#browser-compatibility
+
+module.exports = {
+  // https://cli.vuejs.org/config/#babel
+  presets: [
+    "@vue/app"
+    // https://cli.vuejs.org/guide/browser-compatibility.html#polyfills
+    // , {
+    //   polyfills: [
+    //     'es6.promise',
+    //     'es6.symbol'
+    //   ]
+    // }
+  ]
+  // https://babeljs.io/docs/en/options#sourcetype
+  // , sourceType: "unambiguous",
+};
